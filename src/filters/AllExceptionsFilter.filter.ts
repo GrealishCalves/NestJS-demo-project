4bade5918a9306b4ec29ea8495b2a3e268657963
GrealishCalves <maximmilshtien@gmail.com>
2022-11-04 14:04:06 +0200

    (doc): global filter

--- src/filters/AllExceptionsFilter.filter.ts
+++ src/filters/AllExceptionsFilter.filter.ts
@@ -6,6 +6,12 @@ import * as fs from 'fs';
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
+  /**
+   * It takes an exception and an arguments host, and then it returns a JSON response with the error
+   * message, a timestamp, and a UUID
+   * @param {HttpException} exception - The exception that was thrown.
+   * @param {ArgumentsHost} host - ArgumentsHost: This is the host of the arguments.
+   */
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
@@ -13,13 +19,18 @@ export class AllExceptionsFilter implements ExceptionFilter {
     const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
 
     response.status(status).json({
-      error: this.getResponseLogger(exception),
+      error: this.getResponseError(exception),
       timestamp: new Date().toISOString(),
       uuid: randomUUID(),
     });
   }
 
-  private getResponseLogger(exception: HttpException): IError {
+  /**
+   * It takes an exception, gets the response, writes a log, and returns the response
+   * @param {HttpException} exception - The exception that was thrown.
+   * @returns The error object is being returned.
+   */
+  private getResponseError(exception: HttpException): IError {
     const error: IError = exception.getResponse() as IError;
 
     const log = {
@@ -32,6 +43,10 @@ export class AllExceptionsFilter implements ExceptionFilter {
     return error;
   }
 
+  /**
+   * It takes a log object, converts it to a string, and appends it to a file called Errors.log
+   * @param {any} log - The log object that you want to write to the log file.
+   */
   private writeLog(log: any): void {
     const data = `${JSON.stringify(log)}\r\n`;
     fs.appendFile('Errors.log', data, 'utf8', (err) => {
