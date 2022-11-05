593c14db08d348492d5338615033c8c2e256047f
GrealishCalves <maximmilshtien@gmail.com>
2022-11-05 18:30:01 +0200

    (fix): interceptor shouldn't log

--- src/interceptors/translation.interceptor.ts
+++ src/interceptors/translation.interceptor.ts
@@ -15,8 +15,6 @@ export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> 
   intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
     return next.handle().pipe(
       map((data) => {
-        this.logger(context);
-
         return {
           data: instanceToPlain(data),
           statusCode: context.switchToHttp().getResponse().statusCode,
@@ -25,49 +23,5 @@ export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> 
         };
       }),
     );
-  }
-
-  /**
-   * It takes the request and response objects from the context, and then creates a log object with the
-   * request and response data
-   * @param {ExecutionContext} context - ExecutionContext - The context of the request.
-   */
-  private logger(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-    const response = context.switchToHttp().getResponse();
-    const { method, url } = request;
-    const { statusCode } = response;
-
-    const log = {
-      method,
-      url,
-      statusCode,
-      timestamp: new Date().toISOString(),
-      request: {
-        body: request.body,
-        params: request.params,
-        query: request.query,
-      },
-      //   response: {
-      //     body: response.body,
-      //     params: response.params,
-      //     query: response.query,
-      //   },
-    };
-
-    this.writeLog(log);
-  }
-
-  /**
-   * It takes a log object, converts it to a string, and appends it to a file
-   * @param {any} log - The log object that you want to write to the log file.
-   */
-  private writeLog(log: any) {
-    const data = `${JSON.stringify(log)}\r\n`;
-    fs.appendFile('Logs.log', data, 'utf8', (err) => {
-      if (err) {
-        console.log(err);
-      }
-    });
   }
 }
