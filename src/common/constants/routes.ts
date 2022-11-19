7e4ae71dce12c34c9f4415e03c6f2f029a27d35c
GrealishCalves <maximmilshtien@gmail.com>
2022-11-19 08:30:54 +0200

    (ref): routes constant

---
+++ src/common/constants/routes.ts
@@ -0,0 +1,6 @@
+export enum Routes {
+  GETUSER = 'getUser',
+  CREATEUSER = 'createUser',
+  LOGIN = 'login',
+  REGISTER = 'register',
+}
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Get } from '@nestjs/common';
 import { Post } from '@nestjs/common';
 import { Controller, Inject } from '@nestjs/common';
-import { Routs } from 'src/common/constants/routs';
+import { Routes } from 'src/common/constants/routes';
 import { Services } from 'src/common/constants/services';
 import { CreateUserDto } from './dto/create-user.dto';
 import { IUserService } from './interface/IUserService';
@@ -10,13 +10,13 @@ import { IUserService } from './interface/IUserService';
 export class UserController {
   constructor(@Inject(Services.USERS) private readonly userService: IUserService) {}
 
-  @Get(Routs.GETUSER)
+  @Get(Routes.GETUSER)
   async getUser() {
     // return await this.userService.findUser({ username: 'tests' });
     return { test: 'test' };
   }
 
-  @Post(Routs.CREATEUSER)
+  @Post(Routes.CREATEUSER)
   async createUser(@Body() createUserDto: CreateUserDto) {
     return await this.userService.createUser(createUserDto);
   }
